47c7169116829dd321292831f25e0bc9b2983698
nick19750629 <34128649+nick19750629@users.noreply.github.com>
2018-03-24 21:39:16 +0800

    2018/3/24 学习依赖注入第一天

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '基本的依赖注入样例';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import {ProductService} from './shared/product.service';
+import { Product1Component } from './product1/product1.component';
+import {LoggerService} from './shared/logger.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    Product1Component
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProductService, LoggerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
