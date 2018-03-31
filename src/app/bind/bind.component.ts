e8fcaa66d43846f91b1601faaf47056df1390c0e
nick19750629 <34128649+nick19750629@users.noreply.github.com>
2018-03-31 18:51:50 +0800

    2018/3/31 DOM绑定 & HTML属性绑定

--- src/app/bind/bind.component.ts
+++ src/app/bind/bind.component.ts
@@ -7,12 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindComponent implements OnInit {
 
-  constructor() { }
+  imgUrl: String = 'http://via.placeholder.com/820x230';
+  size: number;
+  divClass: String;
+  isBig: boolean;
+  divAnyClass: any = {
+    a: false,
+    b: false,
+    c: false
+  };
+  isDev: boolean;
+  divStyle: any = {
+    color: 'red',
+    background: 'yellow'
+  };
+
+  constructor() {
+    this.isBig = false;
+    this.isDev = true;
+    setTimeout(() => {
+      this.divClass = 'a b c';
+      this.isBig = true;
+      this.divAnyClass = {
+        a: true,
+        b: true,
+        c: true
+      };
+      this.isDev = false;
+      this.divStyle = {
+        color: 'yellow',
+        background: 'red'
+      };
+    }, 3000);
+  }
 
   ngOnInit() {
+    this.size = 2;
   }
 
   doOnClick(event: any) {
     console.log(event);
+  }
+
+  doOnInput(event: any) {
+    console.log(event.target.value);
+    console.log(event.target.getAttribute('value'));
   }
 }
